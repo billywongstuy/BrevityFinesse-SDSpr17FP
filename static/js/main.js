335e262f1c8e93c1e4eafc3bf8442a8a52564189
Julian Atkin <jatkin@stuy.edu>
2017-06-04 23:51:51 -0400

    cleaned up error msg code

--- static/js/main.js
+++ static/js/main.js
@@ -11,40 +11,11 @@ var allClear = function() {
 };
 
 var notClear = (function() {
-
     var isClicked = false;
-
     return function() {
 	if (!isClicked) {
 	    isClicked = true;
 	    $('<div id="alertMsg" class="alert alert-danger">One or more required fields were left incomplete.</div>').hide().prependTo($("#subForm")).fadeIn(500);
 	}
     }
-	//var msg = $('<div id="alertMsg" class="alert alert-danger">One or more required fields were left incomplete.</div>');
-    /*var msg = document.createElement("div");
-    msg.addClass("alert alert-warning");
-    msg.innerHTML = "One or more required fields were left incomplete.";
-    $("*/
-    //var msg = document.getElementById("alertMsg");
-    //msg.fadeIn(500);
-    //$("#alertMsg").fadeIn(500);
-    //document.getElementById("alertMsg").style.visibility = "";
-    //document.getElementById("alertMsg").style.opacity = "1";
-    /*msg.css({
-	"visibility": "",
-	"height": "100%",
-	"opacity": "1"
-	});*/
-
-    msg.hide().prependTo($("#subForm")).fadeIn(500);
-    //msg.style.display = "";
-    
-    /*(function() {
-	msg.style.opacity = "1";
-	})()*/
-    
-    //window.setInterval(function() {msg.style.opacity = "1";},25);
-    
-    //msg.style.opacity = "1";
-    /*$("#alertMsg").css("opacity","1");*/
 })();
